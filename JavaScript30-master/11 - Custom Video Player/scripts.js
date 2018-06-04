7bd6ac7940072949db2c881bce3ac02d19bf0392
JABedford <james.bedford1991@gmail.com>
2018-06-04 08:36:53 +0100

    javascript30 - 11
    skip forward and backward buttons working.

--- JavaScript30-master/11 - Custom Video Player/scripts.js
+++ JavaScript30-master/11 - Custom Video Player/scripts.js
@@ -22,7 +22,10 @@ function updateButton() {
     toggle.textContent = icon;
 }
 
-
+function skip() {
+    console.log(this.dataset.skip);
+    video.currentTime += parseFloat(this.dataset.skip);
+}
 
 
 
@@ -30,9 +33,10 @@ function updateButton() {
 
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
-video.addEventListener('pause', updateButton)
+video.addEventListener('pause', updateButton);
 
 toggle.addEventListener('click', togglePlay);
+skipButtons.forEach(button => button.addEventListener('click', skip));
 
 
 
