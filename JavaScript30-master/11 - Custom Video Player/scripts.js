2bcce56b6654dfe97c3b17bf5450ee7e055aa406
JABedford <james.bedford1991@gmail.com>
2018-06-04 07:19:05 +0100

    javascript30 = 11
    Made a start on custom video player

---
+++ JavaScript30-master/11 - Custom Video Player/scripts.js
@@ -0,0 +1,11 @@
+/* Get the elements */
+
+const player = document.querySelector('.player');
+const video = document.querySelector('.viewer');
+const progress = document.querySelector('.progress');
+const progressBar = document.querySelector('.progress__filled');
+const toggle = document.querySelector('.toggle');
+const skipButtons = document.querySelectorAll('[data-skip]');
+const ranges = document.querySelector('.player__slider');
+
+
